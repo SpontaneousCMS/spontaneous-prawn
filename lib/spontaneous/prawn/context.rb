2133719a271c1a53ea7b6dcd0f756b125d2c3cf4
Garry Hill <garry@magnetised.net>
2016-09-07 13:51:01 +0100

    Return a Pathname rather than a string for assets

--- lib/spontaneous/prawn/context.rb
+++ lib/spontaneous/prawn/context.rb
@@ -2,7 +2,7 @@ module Spontaneous::Prawn
   module Context
     def asset_path(path, options = {})
       asset = _asset_environment.find_assets(path, options).first
-      asset.pathname.to_s
+      asset.pathname#.to_s
     end
   end
 end
