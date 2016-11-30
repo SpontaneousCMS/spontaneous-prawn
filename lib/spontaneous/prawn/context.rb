4d49c464219af4f2383df41954533e43c9528e9f
Garry Hill <garry@magnetised.net>
2016-11-30 16:51:19 +0000

    Use new asset manifests to lookup files

--- lib/spontaneous/prawn/context.rb
+++ lib/spontaneous/prawn/context.rb
@@ -1,12 +1,7 @@
 module Spontaneous::Prawn
   module Context
     def asset_path(path, options = {})
-      case (asset = _asset_environment.find_assets(path, options).first)
-      when ::Sprockets::StaticAsset
-        asset.pathname
-      else
-        asset
-      end
+      asset_manifests.path(path)
     end
 
     def each_para(field, &block)
